303bf0a83e7ab7383ce8d6e5042c83b88144b588
ibnu <ibnu@clloh.com>
2018-11-01 11:44:36 +0700

    add commit

---
+++ application/views/v_footer.php
@@ -0,0 +1,49 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Ngetravel</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Tentang Kami</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Daftar Harga</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Blog</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Galeri</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Services</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Web Design</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">IT Consulting</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Software & Program</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">IT Audit</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Services</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Web Design</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">IT Consulting</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Software & Program</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">IT Audit</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2018 by All Rights Reserved.</p>
+			</div>
+
+		</div>
+	</footer>
